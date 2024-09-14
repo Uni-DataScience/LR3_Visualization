fd27be537110522aa4f38d244b120070580e2e45
Halyna Melnyk <39273210+mehalyna@users.noreply.github.com>
2024-09-14 16:35:50 +0300

    Update task2_key_principles.py

--- src/task2_key_principles.py
+++ src/task2_key_principles.py
@@ -12,12 +12,7 @@ def create_scatter_plot(data):
     data (DataFrame): A DataFrame containing 'x' and 'y' columns.
     """
     fig, _ = plt.subplots()
-    sns.scatterplot(x='x', y='y', data=data)
-    plt.title('Scatter Plot of X vs Y')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.grid(True)
-    plt.show()
+    pass
     return fig
 
 
