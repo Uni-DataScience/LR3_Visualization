4e5b09d6ee354ca94d5d0d14fd3d47612f834e75
Halyna Melnyk <39273210+mehalyna@users.noreply.github.com>
2024-09-14 16:34:31 +0300

    Initial commit

---
+++ src/task1_visualization.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import collections
+
+
+def plot_distribution(data):
+    """
+    Plots the distribution of data using a bar chart.
+
+    Parameters:
+    data (array-like): An array of categorical data items.
+    """
+    counts = collections.Counter(data)
+    fig, _ = plt.subplots()
+    plt.bar(counts.keys(), counts.values())
+    plt.xlabel('Category')
+    plt.ylabel('Frequency')
+    plt.title('Data Distribution')
+    plt.show()
+    return fig
+
+
+# Example data
+data = np.random.choice(['A', 'B', 'C'], size=100)
+plot_distribution(data)
